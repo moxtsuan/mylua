5359ee61d08e31542276ca5fcaf5aaa8b24902ec
moxtsuan <moxtsuan@users.noreply.github.com>
2016-07-08 11:24:42 +0900

    Can use args

--- rev.lua
+++ rev.lua
@@ -1,10 +1,24 @@
 nyagos.alias.rev = function(args)
-    local str
-    local t
+  local str
+  local t
+  local f
+  local i
+  if #args < 1 then
     while true do
+      t = io.read()
+      if t == nil then break end
+      str = string.reverse(t)
+      print(str)
+    end
+  else
+    for i=1, #args, 1 do
+      io.input(args[i])
+      while true do
         t = io.read()
         if t == nil then break end
         str = string.reverse(t)
-        nyagos.write(str)
+        print(str)
+      end
     end
+  end
 end
